169c408b20400ae53c3cd44a98455db81b7f7323
Wellysson Rocha <33838737+Werisu@users.noreply.github.com>
2022-05-19 20:34:56 -0300

    filtro de cursos na tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { StarComponent } from './star/star.component';
+import { CourseListComponent } from './courses/course-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,37 @@
+import { CourseService } from './course.service';
+import { Component } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+  selector: 'app-course-list',
+  templateUrl: './course-list.component.html',
+})
+export class CourseListComponent {
+  filteredCourses: Course[] = [];
+
+  _courses: Course[] = [];
+
+  _filterBy!: string;
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filterBy = value;
+
+    this.filteredCourses = this._courses.filter(
+      (course: Course) =>
+        course.name
+          .toLocaleLowerCase()
+          .indexOf(this._filterBy.toLocaleLowerCase()) > -1
+    );
+  }
+
+  get filter(): string {
+    return this._filterBy;
+  }
+}
